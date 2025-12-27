ef9eee529f65e496ce85738beb729f1167e14a21
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-27 16:46:47 +0100

    feat: add footer
    Footer component

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Footer = () => (
+  <footer style={{ padding: "2rem", textAlign: "center", color: "#666" }}>
+    <p>&copy; 2024 Biometric Key Project</p>
+  </footer>
+);
